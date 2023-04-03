15e54216f09451e5595e4892baa11282baa4bc64
yuliia-zheliakova <121395063+yuliia-zheliakova@users.noreply.github.com>
2023-04-03 20:22:38 +0200

    Added date
    + found a bag in js and fixed it

--- src/app.js
+++ src/app.js
@@ -1,10 +1,31 @@
 // the auto-resizible input bar
-
 function autoresize() {
   let size = input.scrollWidth;
   input.style.width = size + "px";
   input.style.transition = "none";
 }
 
-let input = document.getElementById("#city-input");
+let input = document.getElementById("city-input");
 input.addEventListener("input", autoresize);
+
+// Display the date
+function getCurFullDate() {
+  const curDate = document.querySelector("#date");
+  const now = new Date();
+  const days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const curFullDate = `${days[now.getDay()]}, ${now.getHours()}:${
+    (now.getMinutes() < 10 ? "0" : "") + now.getMinutes()
+  }`;
+  curDate.textContent = curFullDate;
+}
+
+getCurFullDate();
