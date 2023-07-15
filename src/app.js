dc6d2aab4f223b4d28262f99945368d967913ee6
yuliia-zheliakova <121395063+yuliia-zheliakova@users.noreply.github.com>
2023-07-15 10:37:38 +0200

    Added weather forecast API

--- src/app.js
+++ src/app.js
@@ -36,7 +36,8 @@ function changeIcon(response) {
 }
 
 // Display the weather forecast
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
@@ -60,6 +61,13 @@ function displayForecast() {
 
   forecastHTML += `</div>`;
   forecastElement.innerHTML = forecastHTML;
+}
+
+// Get the forecast date
+function getForecast(coordinates) {
+  let keyApi = "203fa770242fcd2b9555d832a88ea567";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${keyApi}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 // Display the weather
@@ -91,10 +99,8 @@ function currentWeather(response) {
   //let iconApiKey = "e10347bca54258b03todcf0244c0fb2a";
   //let iconUrlApi = `https://api.shecodes.io/weather/v1/current?query=${response.data.name}&key=${iconApiKey}`;
   //axios.get(iconUrlApi).then(changeIcon);
-
-  displayForecast();
-
   getCurFullDate();
+  getForecast(response.data.coord);
 }
 
 document.querySelector("#city-input").addEventListener("keydown", function (e) {
